dd04b94428d9f94a9d157f3871026138564a1f64
Maximilian Meister <mmeister@suse.de>
2018-02-28 13:37:58 +0100

    add controller specs
    feature#registry-settings
    
    Signed-off-by: Maximilian Meister <mmeister@suse.de>

---
+++ spec/controllers/settings/registries_controller_spec.rb
@@ -0,0 +1,80 @@
+require "rails_helper"
+
+# rubocop:disable RSpec/AnyInstance
+RSpec.describe Settings::RegistriesController, type: :controller do
+  before do
+    create(:registry)
+  end
+
+  describe "GET #index" do
+    it "populates an array of registries" do
+      get :index
+      expect(assigns(:registries)).to_not be_empty
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new Registry to @registry" do
+      get :new
+      expect(assigns(:registry)).to be_a(Registry)
+      expect(assigns(:cert)).to be_a(Certificate)
+    end
+
+    it "assigns a new Certificate to @cert" do
+      get :new
+      expect(assigns(:cert)).to be_a(Certificate)
+    end
+  end
+
+  describe "POST #create" do
+    let(:valid_registry_params) do
+      {
+        registry: {
+          name:        "reggy",
+          url:         "https://testing.reg.url",
+          certificate: nil
+        }
+      }
+    end
+
+    context "with valid attributes" do
+      it "saves the new registry in the database" do
+        post :create, valid_registry_params
+        expect(Registry.find_by(name: "reggy")).to be_a(Registry)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new registry in the database" do
+        expect {
+          post :create, valid_registry_params.tap { |p| p[:registry][:url] = "invalid" }
+        }.to_not change { Registry.count }
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    let(:update_registry_params) do
+      {
+        name:        "updated_reggy",
+        url:         "https://testing.reg.url",
+        certificate: nil
+      }
+    end
+
+    it "updates a registry" do
+      reg = Registry.create(name: "reggy", url: "http://some.url")
+      put :update, id: reg.id, registry: update_registry_params
+      expect(Registry.find(reg.id).name).to eq("updated_reggy")
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes a Registry" do
+      expect {
+        delete :destroy, id: Registry.first
+      }.to change { Registry.count }
+    end
+  end
+end
+# rubocop:enable RSpec/AnyInstance
---
+++ spec/controllers/settings/registry_mirrors_controller_spec.rb
@@ -0,0 +1,84 @@
+require "rails_helper"
+
+# rubocop:disable RSpec/AnyInstance
+RSpec.describe Settings::RegistryMirrorsController, type: :controller do
+  before do
+    create(:registry)
+    create(:registry_mirror)
+  end
+
+  describe "GET #index" do
+    it "populates an array of registry mirrors" do
+      get :index
+      expect(assigns(:grouped_mirrors)).to be_present
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new RegistryMirror to @registry_mirror" do
+      get :new
+      expect(assigns(:registry_mirror)).to be_a(RegistryMirror)
+      expect(assigns(:cert)).to be_a(Certificate)
+    end
+
+    it "assigns a new Certificate to @cert" do
+      get :new
+      expect(assigns(:cert)).to be_a(Certificate)
+    end
+  end
+
+  describe "POST #create" do
+    let(:registry) { create(:registry) }
+    let(:valid_registry_mirror_params) do
+      {
+        registry_mirror: {
+          name:        "reggy_mirror",
+          url:         "https://testing.reg.mirror.local",
+          certificate: nil,
+          registry_id: registry.id
+        }
+      }
+    end
+
+    context "with valid attributes" do
+      it "saves the new registry in the database" do
+        post :create, valid_registry_mirror_params
+        expect(RegistryMirror.find_by(name: "reggy_mirror")).to be_a(RegistryMirror)
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not save the new registry in the database" do
+        expect {
+          post :create, valid_registry_mirror_params.tap { |p| p[:registry_mirror][:url] = "invalid" }
+        }.to_not change { RegistryMirror.count }
+      end
+    end
+  end
+
+  describe "PATCH #update" do
+    let(:registry) { Registry.first }
+    let(:update_registry_mirror_params) do
+      {
+        name:        "updated_reggy_mirror",
+        url:         "https://testing.reg.mirror.local",
+        certificate: nil
+      }
+    end
+
+    it "updates a registry mirror" do
+      mirror = RegistryMirror.create(name: "reggy_mirror", url: "http://some.url", registry_id: registry.id)
+      put :update, id: mirror.id, registry_mirror: update_registry_mirror_params
+      expect(RegistryMirror.find(mirror.id).name).to eq("updated_reggy_mirror")
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "deletes a RegistryMirror" do
+      expect {
+        delete :destroy, id: RegistryMirror.first
+      }.to change { RegistryMirror.count }
+    end
+  end
+end
+# rubocop:enable RSpec/AnyInstance
